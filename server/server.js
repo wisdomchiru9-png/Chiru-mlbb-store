f064e39dd102dfa67bd9b526f5b1e1bf8471cd75
wisdomchiru9-png <wisdomchiru9@gmail.com>
2026-03-08 21:18:00 +0530

    serve frontened

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const orderRoutes = require("./routes/order");
 
@@ -8,18 +9,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("Chiru MLBB Store API running");
+/* API ROUTES */
+app.use("/api", orderRoutes);
+
+/* SERVE FRONTEND */
+app.use(express.static(path.join(__dirname, "../client")));
+
+/* HEALTH CHECK */
+app.get("/health", (req, res) => {
+    res.json({ status: "ok" });
 });
 
-app.get("/health",(req,res)=>{
-    res.json({status:"ok"});
+/* LOAD WEBSITE */
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
-app.use("/api",orderRoutes);
-
+/* START SERVER */
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
